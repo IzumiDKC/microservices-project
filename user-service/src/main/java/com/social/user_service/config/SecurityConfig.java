50340c3306254d4022951b136b5a4d0b7188c444
IzumiDKC <huudien111@gmail.com>
2025-12-07 02:02:46 +0700

    feat: add feature Comment for Post and API

--- user-service/src/main/java/com/social/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/social/user_service/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // Cho phép tải báo cáo user tự do
                         .requestMatchers("/api/users/report/**").permitAll()
+                        .requestMatchers("/api/users/search").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
