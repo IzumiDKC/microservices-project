2c4c758a76b6396255c3ed52b2b578b13a80c5f5
IzumiDKC <huudien111@gmail.com>
2025-12-26 04:16:05 +0700

    Update the image reading APIs for the frontend Add .gitignore configuration

--- user-service/src/main/java/com/social/user_service/service/UserService.java
+++ user-service/src/main/java/com/social/user_service/service/UserService.java
@@ -4,6 +4,8 @@ import com.social.user_service.entity.*;
 import com.social.user_service.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Optional;
@@ -12,10 +14,12 @@ import java.util.Optional;
 public class UserService {
     private final AppUserRepository userRepo;
     private final FollowRepository followRepo;
+    private final CloudinaryService cloudinaryService;
 
-    public UserService(AppUserRepository userRepo, FollowRepository followRepo) {
+    public UserService(AppUserRepository userRepo, FollowRepository followRepo, CloudinaryService cloudinaryService) {
         this.userRepo = userRepo;
         this.followRepo = followRepo;
+        this.cloudinaryService = cloudinaryService;
     }
     // Tạo user mới
     public AppUser createUser(AppUser user) {
@@ -63,4 +67,20 @@ public class UserService {
                 .orElse("Unknown User");
     }
 
+    public AppUser updateAvatar(String username, MultipartFile file) {
+        AppUser user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Gọi hàm upload lên mây -> nhận về link https://res.cloudinary.com/...
+        String avatarUrl = cloudinaryService.uploadAvatar(file);
+
+        user.setAvatarUrl(avatarUrl);
+
+        return userRepo.save(user);
+    }
+
+    public AppUser getUserInfo(String username) {
+        return userRepo.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
