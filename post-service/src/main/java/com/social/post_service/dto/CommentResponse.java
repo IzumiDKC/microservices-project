efe802513101e816bad10b069843726ff6855081
IzumiDKC <huudien111@gmail.com>
2025-12-29 00:19:30 +0700

    Create a new DTO and map from Entity to DTO.

---
+++ post-service/src/main/java/com/social/post_service/dto/CommentResponse.java
@@ -0,0 +1,35 @@
+package com.social.post_service.dto;
+
+import java.time.LocalDateTime;
+
+public class CommentResponse {
+    private Long id;
+    private String content;
+    private Long userId;
+    private Long postId;
+    private LocalDateTime createdAt;
+
+    private String username;
+    private String avatarUrl;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getPostId() { return postId; }
+    public void setPostId(Long postId) { this.postId = postId; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getAvatarUrl() { return avatarUrl; }
+    public void setAvatarUrl(String avatarUrl) { this.avatarUrl = avatarUrl; }
+}
