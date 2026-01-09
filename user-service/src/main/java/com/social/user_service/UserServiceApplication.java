9babb53c47430a00d611176b0f2bc21e017c8b58
IzumiDKC <huudien111@gmail.com>
2026-01-09 14:57:35 +0700

    clean code

--- user-service/src/main/java/com/social/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/social/user_service/UserServiceApplication.java
@@ -1,6 +1,6 @@
 package com.social.user_service;
 
-import io.github.cdimascio.dotenv.Dotenv; // Import thư viện này
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
