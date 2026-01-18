07abbf45eda9cbb6b5899a27f85343a5b1f2fa93
IzumiDKC <huudien111@gmail.com>
2026-01-18 16:20:46 +0700

    config for chat service

---
+++ chat-service/src/main/java/com/social/chat_service/ChatServiceApplication.java
@@ -0,0 +1,16 @@
+package com.social.chat_service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(ChatServiceApplication.class, args);
+	}
+
+}
